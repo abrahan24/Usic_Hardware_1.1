61b3cbb4f9f6bbbf3a221ad294d834ca12718d26
abrahan24 <asaraviacoca@gmail.com>
2023-10-04 16:56:31 -0400

    update

--- src/main/java/com/hardware/SystemUsic/controllers/BajaController.java
+++ src/main/java/com/hardware/SystemUsic/controllers/BajaController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.hardware.SystemUsic.models.entity.Almacen;
@@ -51,20 +52,39 @@ public class BajaController {
 		}
     }
     
-    @RequestMapping(value = "/activos/{id_almacen}")
-	public String getContent1(@PathVariable(value = "id_almacen")Long [] id_almacen, Model model, HttpServletRequest request){
+    // @RequestMapping(value = "/activos/{id_almacen}")
+	// public String getContent1(@PathVariable(value = "id_almacen")Long [] id_almacen, Model model, HttpServletRequest request){
 	
-        System.out.println(id_almacen);
-        List<Long> idsSeleccionados; 
+    //     System.out.println(id_almacen);
+    //     List<Long> idsSeleccionados; 
 
-        for (int i = 0; i < id_almacen.length; i++) {
-            idsSeleccionados = Arrays.asList(id_almacen[i]);
-            model.addAttribute("activos", almacenService.Lista_Activos_Por_Id(idsSeleccionados));
-            System.out.println(almacenService.Lista_Activos_Por_Id(idsSeleccionados));
+    //     for (int i = 0; i < id_almacen.length; i++) {
+    //         idsSeleccionados = Arrays.asList(id_almacen[i]);
+    //         System.out.println(idsSeleccionados.get(i));
+    //         model.addAttribute("activos", almacenService.Lista_Activos_Por_Id(idsSeleccionados));
+    //         System.out.println(almacenService.Lista_Activos_Por_Id(idsSeleccionados));
 
-        }
+    //     }
        
-		return "content :: content2";
+	// 	return "content :: content2";
 
-	}
+	// }
+
+    @RequestMapping(value = "/activos")
+    public String getContent1(@RequestParam(value = "selectedValues", required = false) String selectedValues, Model model, HttpServletRequest request) {
+        if (selectedValues != null) {
+            // Dividir la cadena de valores separados por comas en un array de Long
+            String[] selectedValuesArray = selectedValues.split(",");
+            Long[] id_almacen = new Long[selectedValuesArray.length];
+
+            for (int i = 0; i < selectedValuesArray.length; i++) {
+                id_almacen[i] = Long.parseLong(selectedValuesArray[i]);
+            }
+
+            List<Long> idsSeleccionados = Arrays.asList(id_almacen);
+            model.addAttribute("activos", almacenService.Lista_Activos_Por_Id(idsSeleccionados));
+        }
+
+        return "content :: content2";
+    }
 }
